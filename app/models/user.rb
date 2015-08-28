db79628c18a0a495346d7cc1337a65153570979d
Perry Carbone <perrycarbone@Perrys-MBP.home>
2015-08-28 14:11:38 -0400

    added slug and voteable modules and voteable gem.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  include Sluggable
+
   has_many :posts
   has_many :comments
   has_many :votes
@@ -9,36 +11,6 @@ class User < ActiveRecord::Base
   validates :password, presence: true, on: :create, length: {minimum: 5}
   validates :time_zone, presence: true
 
-  before_save :generate_slug!
+  sluggable_column :username
 
-  def generate_slug!
-    the_slug = to_slug(self.username)
-    user = User.find_by slug: the_slug
-    count = 2
-    while user && user != self
-      the_slug = append_suffix(the_slug, count)
-      user = User.find_by(slug: the_slug)
-      count += 1
-    end
-    self.slug = the_slug.downcase
-  end
-
-  def append_suffix(str, count)
-    if str.split('-').last.to_i != 0
-      return str.split('-').slice(0...-1).join('-') + "-" + count.to_s
-    else
-      return str + "-" + count.to_s
-    end
-  end
-
-  def to_slug(name)
-    str = name.strip
-    str.gsub! /\s*[^A-Za-z0-9]\s*/, '-'
-    str.gsub! /-+/, "-"
-    str.downcase
-  end
-
-  def to_param
-    self.slug
-  end
 end
